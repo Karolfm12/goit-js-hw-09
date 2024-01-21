9260069ed121bf1d419f5988e14c7d5ff7e0e5a3
Karolfm12 <karolfm12@onet.eu>
2024-01-21 12:35:19 +0100

    1completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+stopButton.disabled = true;
+startButton.addEventListener('click', e => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+});
+
+stopButton.addEventListener('click', e => {
+  clearInterval(timerId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

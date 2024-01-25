71e239e05d67d2f62988e3d6f1f335c89cac8de7
Karolfm12 <karolfm12@onet.eu>
2024-01-25 21:31:40 +0100

    promise1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,28 @@
-import Notiflix from 'notiflix';
+const firstDelay = document.querySelector('[name=delay]');
+const delayStep = document.querySelector('[name=step]');
+const amount = document.querySelector('[name="amount"]');
+const button = document.querySelector('[type="submit"]');
+
+button.addEventListener('click', e => {
+  e.preventDefault();
+});
 
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {});
+  });
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
-    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
-    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
   }
 }
 
 createPromise(2, 1500)
   .then(({ position, delay }) => {
     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
   });

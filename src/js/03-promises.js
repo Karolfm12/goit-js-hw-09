929ca1baf05ba4df84279e4db97da33661e4ee15
Karol Kotański <karolfm12@onet.eu>
2024-01-26 10:45:35 +0100

    promise3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,28 +1,36 @@
 const firstDelay = document.querySelector('[name=delay]');
 const delayStep = document.querySelector('[name=step]');
-const amount = document.querySelector('[name="amount"]');
+const amountInput = document.querySelector('[name="amount"]');
 const button = document.querySelector('[type="submit"]');
 
 button.addEventListener('click', e => {
   e.preventDefault();
+  const amount = Number(amountInput.value);
+  const startDelay = Number(firstDelay.value);
+  const step = Number(delayStep.value);
+
+  for (let i = 1; i <= amount; i++) {
+    const currentDelay = startDelay + (i - 1) * step;
+
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${i} in ${currentDelay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${i} in ${currentDelay}ms`);
+      });
+  }
 });
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {});
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
 }
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
